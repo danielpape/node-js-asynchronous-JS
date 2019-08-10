de82f531181ba2bcf4deed02145cbf94272c1cb9
Daniel Pape <danielwpape@gmail.com>
2019-08-10 21:01:31 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find this file');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject('I could not write this file');
+      resolve('success');
+    });
+  });
+};
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('Random dog image saved to file');
+  } catch (err) {
+    console.log('ERROR');
+    throw err;
+  }
+  return '2: READY';
+};
+
+(async () => {
+  try {
+    console.log('1.Will get dog photos');
+    const x = await getDogPic();
+    console.log('3. Done getting dog photos');
+  } catch (err) {
+    console.log('error');
+  }
+})();
+
+// getDogPic().then(x => {
+//   console.log(x);
+// });
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then(data => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then(res => {
+//     console.log(res.body.message);
+//     return writeFilePro('dog-img.txt', res.body.message);
+//   })
+//   .then(() => {
+//     console.log('Random dog image saved to file');
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
